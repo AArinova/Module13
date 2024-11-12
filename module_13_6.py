1dc80daf347cdd5d1a9046934def6654d5088401
AArinova <aliya-arinova@yandex.ru>
2024-11-12 17:49:54 +0700

    Add files via upload

--- module_13_6.py
+++ module_13_6.py
@@ -6,20 +6,21 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-api = ""
+api = "7245377370:AAHM2WCQKtOFuRQzZyuV2MakYowLQgObyyA"
 bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
-# """keyboard"""
-# kb = ReplyKeyboardMarkup(resize_keyboard=True)
-# button = KeyboardButton(text="Рассчитать")
-# button_info = KeyboardButton(text="Информация")
-# kb.row(button, button_info)-
+"""keyboard1"""
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text="Рассчитать")
+button_info = KeyboardButton(text="Информация")
+kb.row(button, button_info)
 
-kb = InlineKeyboardMarkup()
+"""keyboard2"""
+ib = InlineKeyboardMarkup()
 button1 = InlineKeyboardButton(text="Рассчитать норму калорий", callback_data='calories')
 button2 = InlineKeyboardButton(text="Формулы расчёта", callback_data = 'formulas')
-kb.add(button1, button2)
+ib.add(button1, button2)
 
 class UserState(StatesGroup):
     age = State()
@@ -28,15 +29,16 @@ class UserState(StatesGroup):
 
 
 @dp.message_handler(text=['Рассчитать'])
-async def main_menu(call):
-    pass
+async def main_menu(message):
+    await message.answer("Выберите опцию:", reply_markup=ib)
 
 @dp.callback_query_handler(text="formulas")
-
+async def get_formulas(call):
+    await call.message.answer("Ваша норма калорий :", reply_markup=ib)
 
 @dp.callback_query_handler(text='calories')
-async def set_age(call):
-    await call.message('Введите свой возраст:')
+async def set_age(message):
+    await message('Введите свой возраст:')
     await UserState.age.set()
 
 @dp.message_handler(state=UserState.age)
@@ -53,11 +55,12 @@ async def set_weight(call, state):
     
 """кнопка Старт"""
 @dp.message_handler(commands=["start"])
-async def start(call):
-    await call.message.answer("Привет!", reply_markup = kb)
-    await call.answer()
+async def start(message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью", reply_markup = kb)
+    await message.answer()
 
-@dp.message_handler(state=UserState.weight)
+#@dp.message_handler(state=UserState.weight)
+@dp.callback_query_handler(text='calories')
 async def send_calories(call, state):
     await state.update_data(weight=call.message.text)
     await UserState.weight.set()
